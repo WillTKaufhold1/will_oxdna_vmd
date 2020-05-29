bea506fc81b6cc90124496fdf930b124c2d17c16
wtk23 <willtkaufhold@gmail.com>
2020-05-29 11:28:56 +0100

    some commits

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages, Extension
+#todo write up extension
 
-module1 = Extension('oxdna-vmd',
-                    sources = ['oxdna-vmd/ox2xyzmodule.c'])
-
-setup (name = 'oxdna-vmd',
+setup (name = 'oxdnavmd',
        version = '0.1',
        description = 'vmd support for oxdna trajectories',
        author = 'Will T Kaufhold',
        author_email = 'wtk23@cam.ac.uk',
        url = "https://github.com/WillTKaufhold1/will_oxdna_vmd",
-       ext_modules = [module1])
+       licence = "GPL",
+       packages = find_packages()
+)
